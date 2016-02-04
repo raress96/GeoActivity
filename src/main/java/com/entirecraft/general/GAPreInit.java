d569e4262e95a83418470d1390d71f00fcd33b9f
Serban Rares <rares_serban96@yahoo.com>
2016-02-04 21:01:54 +0200

    Removed deprecated NEI handler.(for now)

--- src/main/java/com/entirecraft/general/GAPreInit.java
+++ src/main/java/com/entirecraft/general/GAPreInit.java
@@ -1,16 +1,6 @@
 package com.entirecraft.general;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.client.resources.model.ModelBakery;
-import net.minecraft.item.Item;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
 import com.entirecraft.core.GeoWorld;
-import com.entirecraft.general.blocks.BaseBlock;
 import com.entirecraft.general.blocks.CustomCoalOres;
 import com.entirecraft.general.blocks.HardenedBrick;
 import com.entirecraft.general.blocks.TransparentBlock;
@@ -24,7 +14,6 @@ import com.entirecraft.general.blocks.Machines.GeothermalGenerator;
 import com.entirecraft.general.blocks.Machines.ThermicGenerator;
 import com.entirecraft.general.blocks.Machines.ThermicMelter;
 import com.entirecraft.general.blocks.Machines.ToolCharger;
-import com.entirecraft.general.itemblocks.MultiItemBlock;
 import com.entirecraft.general.items.ArmorPerks;
 import com.entirecraft.general.items.AutoStoneBuilder;
 import com.entirecraft.general.items.BaseItem;
@@ -53,19 +42,28 @@ import com.entirecraft.general.items.tools.Red.RedstoneMiner;
 import com.entirecraft.lib.IHasName;
 import com.entirecraft.lib.Reference;
 
-public class GAPreInit 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.resources.model.ModelBakery;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class GAPreInit
 {
 	public static void preInit(FMLPreInitializationEvent event)
-	{				
+	{
 		registerItems();
 		registerBlocks();
-		
+
 		if (event.getSide() == Side.CLIENT)
 			registerMetadataTextures();
-		
+
 		GameRegistry.registerWorldGenerator(new GeoWorld(), 2);
 	}
-	
+
 	private static void registerItems()
 	{
 		/* Items */
@@ -77,19 +75,19 @@ public class GAPreInit
 		GAMod.carbonfiber = new BaseItem("carbonfiber");
 		GAMod.carbonstick = new BaseItem("carbonstick");
 		GAMod.carbonOreCutter = new CarbonOreCutter("carbonorecutter");
-		
+
 		GAMod.preciousAlloy = new PreciousAlloy("preciousalloyingot");
-		
+
 		GAMod.tool_perks = new ToolPerks("perk");
 		GAMod.machine_perks = new MachinePerks("perk_m");
 		GAMod.armor_perks = new ArmorPerks("perk_a");
-		
+
 		GAMod.elementContainer = new ElementContainer("elementcontainer");
-		
+
 		GAMod.autoSBuilder = new AutoStoneBuilder("autostonebuilder");
-		
+
 		/* Tools */
-		
+
 		//Tier I
 		GAMod.reinforcedSword = new ReinforcedSword("reinforcedsword");
 		GAMod.reinforcedPickaxe = new ReinforcedPickaxe("reinforcedpickaxe");
@@ -97,27 +95,27 @@ public class GAPreInit
 		GAMod.reinforcedShovel = new ReinforcedShovel("reinforcedshovel");
 		GAMod.reinforcedHoe = new ReinforcedHoe("reinforcedhoe");
 		GAMod.reinforcedMiner = new ReinforcedMiner("reinforcedminer");
-		
+
 		//Tier II
 		GAMod.advancedSword = new AdvSword("advancedsword");
 		GAMod.advancedPickaxe = new AdvPick("advancedpick");
 		GAMod.advancedAxe = new AdvAxe("advancedaxe");
 		GAMod.advancedShovel = new AdvShovel("advancedshovel");
-		
+
 		//Tier III
 		GAMod.redstoneMiner = new RedstoneMiner("redstoneminer");
 		GAMod.redstoneBattleAxe = new RedstoneBattleAxe("redstonebattleaxe");
 		GAMod.redstoneBattleMiner = new RedstoneBattleMiner("redstonebattleminer");
 		GAMod.aredstoneBattleMiner = new ARedstoneBattleMiner("aredstonebattleminer");
-		
+
 		/* Armor */
-		
+
 		//Tier I
 		GAMod.reinforcedHelmet = new ReinforcedArmor("reinforcedhelmet", 1, 0);
 		GAMod.reinforcedChest = new ReinforcedArmor("reinforcedchest", 1, 1);
 		GAMod.reinforcedPants = new ReinforcedArmor("reinforcedpants", 2, 2);
 		GAMod.reinforcedBoots = new ReinforcedArmor("reinforcedboots", 1, 3);
-		
+
 		//Tier II
 		GAMod.advancedHelmet = new AdvancedArmor("advancedhelmet", 1, 0);
 		GAMod.advancedChest = new AdvancedArmor("advancedchest", 1, 1);
@@ -131,30 +129,30 @@ public class GAPreInit
 		GAMod.oreLignite = new CustomCoalOres("ligniteore", 2).setHardness(3.0F);
 		GAMod.oreBituminous = new CustomCoalOres("bituminousore", 2).setHardness(4.0F);
 		GAMod.oreAnthracite = new CustomCoalOres("anthraciteore", 3).setHardness(5.0F);
-		
+
 		//General Blocks
 		GAMod.hardenedglass = new TransparentBlock("hardenedglass", Material.glass, 3).setHardness(20.0F).setResistance(130.0F);
 		GAMod.graphitebase = new TransparentBlock("graphitebase", Material.rock, 2).setHardness(3.0F).setResistance(15.0F);
-		
+
 		GAMod.hardenedbrick = new HardenedBrick("hardenedbrick");
-		
+
 		/* Machines */
-		
+
 		//Tier I
 		GAMod.coalrefiner = new CoalRefiner("coalrefiner");
 		GAMod.craftingmachine = new CraftingMachine("craftingmachine");
-		
+
 		//Tier II
 		GAMod.atomextractor = new AtomExtractor("atomextractor");
 		GAMod.chemistrymachine = new ChemistryMachine("chemistrymachine");
 		GAMod.advancedcoalrefiner = new AdvancedCoalRefiner("advancedcoalrefiner");
 		GAMod.advancedorerefiner = new AdvancedOreRefiner("advancedorerefiner");
-		
+
 		GAMod.thermicgenerator = new ThermicGenerator("thermicgenerator");
 		GAMod.toolcharger = new ToolCharger("toolcharger");
 		GAMod.geothermalgenerator = new GeothermalGenerator("geothermalgenerator");
 		GAMod.thermicmelter = new ThermicMelter("thermicmelter");
-		
+
 	}
 
 	private static void registerBlock(Block block, String uname, String tool, int tier)
@@ -162,27 +160,27 @@ public class GAPreInit
 		GameRegistry.registerBlock(block, Reference.MOD_ID + uname);
 		block.setHarvestLevel(tool, tier);
 	}
-	
+
 	@SideOnly(Side.CLIENT)
 	private static void registerMetadataTextures()
 	{
 		int i;
-		
+
 		for(i = 0;i < PreciousAlloy.alloy_names.length;i++)
 			ModelBakery.addVariantName(GAMod.preciousAlloy, Reference.MOD_ID + ":" + ((IHasName)GAMod.preciousAlloy).getName(i));
-				
+
 		for(i = 0;i < ToolPerks.EnumToolPerks.values().length;i++)
 			ModelBakery.addVariantName(GAMod.tool_perks, Reference.MOD_ID + ":" + ((IHasName)GAMod.tool_perks).getName(i));
-		
+
 		for(i = 0;i < MachinePerks.EnumMachinePerks.values().length;i++)
 			ModelBakery.addVariantName(GAMod.machine_perks, Reference.MOD_ID + ":" + ((IHasName)GAMod.machine_perks).getName(i));
-		
+
 		for(i = 0;i < ArmorPerks.EnumArmorPerks.values().length;i++)
 			ModelBakery.addVariantName(GAMod.armor_perks, Reference.MOD_ID + ":" + ((IHasName)GAMod.armor_perks).getName(i));
-		
+
 		for(i = 0;i < ElementContainer.EnumElements.values().length;i++)
 			ModelBakery.addVariantName(GAMod.elementContainer, Reference.MOD_ID + ":" + ((IHasName)GAMod.elementContainer).getName(i));
-		
+
 		Item temp = GameRegistry.findItem(Reference.MOD_ID, ((IHasName)GAMod.hardenedbrick).getName());
 		for(i = 0;i < HardenedBrick.EnumHardenedBrick.values().length;i++)
 			ModelBakery.addVariantName(temp, Reference.MOD_ID + ":" + ((IHasName)GAMod.hardenedbrick).getName(i));
