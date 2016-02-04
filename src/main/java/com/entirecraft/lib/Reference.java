eb100f7a0d605714c71074304dc1f67b9011f938
Rares Serban <rares_serban96@yahoo.com>
2016-02-04 23:01:12 +0200

    Update Reference.java

--- src/main/java/com/entirecraft/lib/Reference.java
+++ src/main/java/com/entirecraft/lib/Reference.java
@@ -4,5 +4,5 @@ public class Reference
 {
 	public static final String MOD_ID = "geoactivity";
 	public static final String MOD_NAME = "GeoActivity";
-	public static final String VERSION = "2.1.1";
+	public static final String VERSION = "2.2";
 }
