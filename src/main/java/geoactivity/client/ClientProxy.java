d7d22180f77d80fa02f00b6562fa06f162774367
Serban Rares <rares_serban96@yahoo.com>
2016-02-21 22:48:20 +0200

    Updated RF API and JEI version.Comefully fixed client crash.

--- src/main/java/geoactivity/client/ClientProxy.java
+++ src/main/java/geoactivity/client/ClientProxy.java
@@ -1,9 +1,8 @@
 package geoactivity.client;
 
 import geoactivity.common.CommonProxy;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
 
 public class ClientProxy extends CommonProxy
-{	
+{
 
 }
--- src/main/java/geoactivity/common/blocks/Machines/CrM/recipes/CrMShapedRecipe.java
+++ src/main/java/geoactivity/common/blocks/Machines/CrM/recipes/CrMShapedRecipe.java
@@ -2,22 +2,160 @@ package geoactivity.common.blocks.Machines.CrM.recipes;
 
 import java.util.List;
 
+import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.ShapedRecipes;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
 
-public class CrMShapedRecipe extends ShapedRecipes
+public class CrMShapedRecipe implements IRecipe
 {
+    public final int recipeWidth;
+    public final int recipeHeight;
+    public final ItemStack[] recipeItems;
+    private final ItemStack recipeOutput;
+    private boolean copyIngredientNBT;
 	private List<ItemStack> compatiblePerks;
-	
-	public CrMShapedRecipe(int width, int height, ItemStack[] stack, ItemStack output, List<ItemStack> perks)
+
+	public CrMShapedRecipe(int width, int height, ItemStack[] inputs, ItemStack output, List<ItemStack> perks)
 	{
-		super(width, height, stack, output);
-		
+		this.recipeWidth = width;
+        this.recipeHeight = height;
+        this.recipeItems = inputs;
+        this.recipeOutput = output;
 		this.compatiblePerks = perks;
 	}
-	
+
 	public List<ItemStack> getCompatiblePerks()
 	{
 		return this.compatiblePerks;
 	}
+
+    @Override
+	public ItemStack getRecipeOutput()
+    {
+        return this.recipeOutput;
+    }
+
+    @Override
+	public ItemStack[] getRemainingItems(InventoryCrafting inv)
+    {
+        ItemStack[] aitemstack = new ItemStack[inv.getSizeInventory()];
+
+        for (int i = 0; i < aitemstack.length; ++i)
+        {
+            ItemStack itemstack = inv.getStackInSlot(i);
+            aitemstack[i] = net.minecraftforge.common.ForgeHooks.getContainerItem(itemstack);
+        }
+
+        return aitemstack;
+    }
+
+    /**
+     * Used to check if a recipe matches current crafting inventory
+     */
+    @Override
+	public boolean matches(InventoryCrafting inv, World worldIn)
+    {
+        for (int i = 0; i <= 3 - this.recipeWidth; ++i)
+        {
+            for (int j = 0; j <= 3 - this.recipeHeight; ++j)
+            {
+                if (this.checkMatch(inv, i, j, true))
+                {
+                    return true;
+                }
+
+                if (this.checkMatch(inv, i, j, false))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks if the region of a crafting inventory is match for the recipe.
+     */
+    private boolean checkMatch(InventoryCrafting p_77573_1_, int p_77573_2_, int p_77573_3_, boolean p_77573_4_)
+    {
+        for (int i = 0; i < 3; ++i)
+        {
+            for (int j = 0; j < 3; ++j)
+            {
+                int k = i - p_77573_2_;
+                int l = j - p_77573_3_;
+                ItemStack itemstack = null;
+
+                if (k >= 0 && l >= 0 && k < this.recipeWidth && l < this.recipeHeight)
+                {
+                    if (p_77573_4_)
+                    {
+                        itemstack = this.recipeItems[this.recipeWidth - k - 1 + l * this.recipeWidth];
+                    }
+                    else
+                    {
+                        itemstack = this.recipeItems[k + l * this.recipeWidth];
+                    }
+                }
+
+                ItemStack itemstack1 = p_77573_1_.getStackInRowAndColumn(i, j);
+
+                if (itemstack1 != null || itemstack != null)
+                {
+                    if (itemstack1 == null && itemstack != null || itemstack1 != null && itemstack == null)
+                    {
+                        return false;
+                    }
+
+                    if (itemstack.getItem() != itemstack1.getItem())
+                    {
+                        return false;
+                    }
+
+                    if (itemstack.getMetadata() != 32767 && itemstack.getMetadata() != itemstack1.getMetadata())
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns an Item that is the result of this recipe
+     */
+    @Override
+	public ItemStack getCraftingResult(InventoryCrafting inv)
+    {
+        ItemStack itemstack = this.getRecipeOutput().copy();
+
+        if (this.copyIngredientNBT)
+        {
+            for (int i = 0; i < inv.getSizeInventory(); ++i)
+            {
+                ItemStack itemstack1 = inv.getStackInSlot(i);
+
+                if (itemstack1 != null && itemstack1.hasTagCompound())
+                {
+                    itemstack.setTagCompound((NBTTagCompound)itemstack1.getTagCompound().copy());
+                }
+            }
+        }
+
+        return itemstack;
+    }
+
+    /**
+     * Returns the size of the recipe area
+     */
+    @Override
+	public int getRecipeSize()
+    {
+        return this.recipeWidth * this.recipeHeight;
+    }
 }
